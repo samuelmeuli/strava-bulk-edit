cf6daae896c4db61ae19c1c73f9a32b38fa01f82
Samuel Meuli <me@samuelmeuli.com>
2018-11-22 21:54:09 +0100

    Move options to separate file

--- src/main.go
+++ src/main.go
@@ -12,9 +12,6 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var valuesSport = []string{"AlpineSki", "BackcountrySki", "Canoeing", "Crossfit", "EBikeRide", "Elliptical", "Handcycle", "Hike", "IceSkate", "InlineSkate", "Kayaking", "Kitesurf", "NordicSki", "Ride", "RockClimbing", "RollerSki", "Rowing", "Run", "Snowboard", "Snowshoe", "StairStepper", "StandUpPaddling", "Surfing", "Swim", "VirtualRide", "VirtualRun", "Walk", "WeightTraining", "Wheelchair", "Windsurf", "Workout", "Yoga"}
-var valuesVisibility = []string{"only_me", "followers_only", "everyone"}
-
 var (
 	app = kingpin.New("strava-bulk-edit", "Edit multiple Strava activities at once").Version("0.0.1").Author("Samuel Meuli")
 
@@ -32,9 +29,9 @@ var (
 	commute        = app.Command("commute", "Set the activities as commute/non-commute activities")
 	commuteArg     = commute.Arg("commute", "true or false indicating whether activities should be marked commute").Required().Bool()
 	sport          = app.Command("sport", "Update the activities' sports type")
-	sportArg       = sport.Arg("sport", "New sports type for the activities").Required().Enum(valuesSport...)
+	sportArg       = sport.Arg("sport", "New sports type for the activities").Required().Enum(optionsSport...)
 	visibility     = app.Command("visibility", "Update the activities' visibility")
-	visibilityArg  = visibility.Arg("visibility", "New visibility for the activities").Required().Enum(valuesVisibility...)
+	visibilityArg  = visibility.Arg("visibility", "New visibility for the activities").Required().Enum(optionsVisibility...)
 )
 
 func main() {
---
+++ src/options.go
@@ -0,0 +1,38 @@
+package main
+
+var optionsSport = []string{
+	"AlpineSki",
+	"BackcountrySki",
+	"Canoeing",
+	"Crossfit",
+	"EBikeRide",
+	"Elliptical",
+	"Handcycle",
+	"Hike",
+	"IceSkate",
+	"InlineSkate",
+	"Kayaking",
+	"Kitesurf",
+	"NordicSki",
+	"Ride",
+	"RockClimbing",
+	"RollerSki",
+	"Rowing",
+	"Run",
+	"Snowboard",
+	"Snowshoe",
+	"StairStepper",
+	"StandUpPaddling",
+	"Surfing",
+	"Swim",
+	"VirtualRide",
+	"VirtualRun",
+	"Walk",
+	"WeightTraining",
+	"Wheelchair",
+	"Windsurf",
+	"Workout",
+	"Yoga",
+}
+
+var optionsVisibility = []string{"only_me", "followers_only", "everyone"}
